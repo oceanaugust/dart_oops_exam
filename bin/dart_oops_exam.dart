40d7e4f46b7ae8c59dda5797b95c054488e36838
oceanaugust <notalphagameryt@gmail.com>
2025-01-29 12:26:06 +0530

    commit	new file:   .gitignore 	new file:   CHANGELOG.md 	new file:   README.md 	new file:   analysis_options.yaml 	new file:   bin/dart_oops_exam.dart 	new file:   lib/dart_oops_exam.dart 	new file:   pubspec.lock 	new file:   pubspec.yaml 	new file:   test/dart_oops_exam_test.dart

---
+++ bin/dart_oops_exam.dart
@@ -0,0 +1,118 @@
+1.
+
+void squares(int a) {
+  for (int i = 1; i <= a; i++) print(i * i);
+}
+
+void main() {
+  squares(5);
+}
+
+2.
+
+abstract class Animal {
+  makesound();
+}
+
+class Dog extends Animal {
+  @override
+  makesound() {
+    print("Woof");
+  }
+}
+
+class Cat extends Animal {
+  @override
+  makesound() {
+    print("Meow");
+  }
+}
+
+void main() {
+  Dog dog = Dog();
+  Cat cat = Cat();
+
+  dog.makesound();
+  cat.makesound();
+}
+
+3.
+
+class Shape {
+  String _color;
+
+  Shape(this._color);
+  String get color => _color;
+  set color(String newColor) => _color = newColor;
+
+  displayColor() {
+    print("color  : $_color");
+  }
+}
+
+class Circle extends Shape {
+  double _radius;
+  Circle(this._radius, super._color);
+
+  double get radius => _radius;
+
+  displayProperties() {
+    displayColor();
+    print("radius : $radius");
+  }
+}
+
+class Rectangle extends Shape {
+  double _length;
+  double _breath;
+
+  Rectangle(this._length, this._breath, super._color);
+
+  double get legeth => _length;
+  double get breath => _breath;
+
+  displayProperties() {
+    displayColor();
+    print("length : $legeth");
+    print("breath : $breath");
+  }
+}
+
+void main() {
+  Rectangle rec = Rectangle(5, 20, "red");
+  Circle cir = Circle(5, "green");
+
+  cir.displayProperties();
+  rec.displayProperties();
+}
+
+4.
+
+mixin Logger {
+  void logMessage(String message) {
+    print("Log: $message");
+  }
+}
+
+abstract class Vehicle {
+  void startEngine();
+  void stopEngine();
+}
+
+class Car extends Vehicle with Logger {
+  @override
+  void startEngine() {
+    logMessage("Car engine started.");
+  }
+
+  @override
+  void stopEngine() {
+    logMessage("Car engine stopped.");
+  }
+}
+
+void main() {
+  Car myCar = Car();
+  myCar.startEngine();
+  myCar.stopEngine();
+}
---
+++ test/dart_oops_exam_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_oops_exam/dart_oops_exam.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
